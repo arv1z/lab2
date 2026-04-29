7652190b1f06cc4a12093936240f79e911a92471
arv1z <ksskhramov@gmail.com>
2026-04-29 09:21:08 +0300

    Apply Mozilla code style using clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    std:: string name; // переменная имени
-    std:: cout << "Введите имя: ";
-    std::cin >> name; // ввод имени
-    std::cout << "Hello world " << name << "!" << std::endl;
-    return 0;
+int
+main()
+{
+  std::string name; // переменная имени
+  std::cout << "Введите имя: ";
+  std::cin >> name; // ввод имени
+  std::cout << "Hello world " << name << "!" << std::endl;
+  return 0;
 }
-
-
