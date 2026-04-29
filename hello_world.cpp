71d3562a581fd3d007b3f5ef5aab8433b6804a46
arv1z <ksskhramov@gmail.com>
2026-04-29 09:29:08 +0300

    Change greeting in main

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,7 +5,7 @@ int main() {
     std:: string name; // переменная имени
     std:: cout << "Введите имя: ";
     std::cin >> name; // ввод имени
-    std::cout << "Hello world " << name << "!" << std::endl;
+    std::cout << "Hello world from main bracnh,  " << name << "!" << std::endl;
     return 0;
 }
 
