082fab3b321f90d7b9ded32727ab5538d03098a3
arv1z <ksskhramov@gmail.com>
2026-04-29 09:11:45 +0300

    Add comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 int main() {
-    std:: string name;
+    std:: string name; // переменная имени
     std:: cout << "Введите имя: ";
-    std::cin >> name;
+    std::cin >> name; // ввод имени
     std::cout << "Hello world " << name << "!" << std::endl;
     return 0;
 }
